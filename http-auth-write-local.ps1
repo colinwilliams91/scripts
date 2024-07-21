68483086dd11d5b462f4b56d18ae6acb61bf804a
Colin Williams <colin.williams.dev@gmail.com>
2024-07-21 03:56:10 -0500

    feat: Read-Host walkthrough for each param and reqs, working

--- http-auth-write-local.ps1
+++ http-auth-write-local.ps1
@@ -7,17 +7,56 @@
 # --> Writes to specified output file type #############
 ########################################################
 
+# function Export-Rest-Data {
+#     param (
+#         [Parameter(Mandatory=$false)]
+#         [string]$Token = "",
+
+#         [Parameter(Mandatory=$true)]
+#         [string]$Url,
+
+#         [Parameter(Mandatory=$true)]
+#         [string]$OutFile
+#     )
+
+#     if ($Token) {
+#         $headers = @{
+#             Authorization = "Bearer $Token"
+#         }
+#     } else {
+#         $headers = @{}
+#     }
+
+#     try {
+#         $response = Invoke-RestMethod -Uri $Url -Headers $headers
+#         $response | Out-File -FilePath $OutFile -Encoding utf8
+#         Write-Output "Data export successful. File saved as $OutFile."
+#     } catch {
+#         Write-Error "Failed to export data: $_"
+#     }
+# }
+
+##################################################################
+# Same function with User prompts to write vars ##################
+# Expecting the user to know which arguments to pass #############
+##################################################################
+
 function Export-Rest-Data {
-    param (
-        [Parameter(Mandatory=$false)]
-        [string]$Token = "",
+    [string]$Token = "",
+    [string]$Url,
+    [string]$OutFile
 
-        [Parameter(Mandatory=$true)]
-        [string]$Url,
+    $Url = Read-Host -Prompt "`nPlease provide the http(s) target URL endpoint"
 
-        [Parameter(Mandatory=$true)]
-        [string]$OutFile
-    )
+    Write-Output "`nIf you need to provide an Authorization Bearer Token..."
+    Write-Output 'It will be added to the Headers like this: { Authorization = Bearer $Token }'
+
+    $Token = Read-Host -Prompt "`nPlease provide your token as a string or press enter to skip"
+
+    Write-Output = "`nPlease provide the full file path where you would like to write the response..."
+    Write-Output = 'The local file will default to your CWD if not specified...'
+
+    $OutFile = Read-Host -Prompt "`nInclude the file extension type you would like to encode to"
 
     if ($Token) {
         $headers = @{
@@ -30,8 +69,13 @@ function Export-Rest-Data {
     try {
         $response = Invoke-RestMethod -Uri $Url -Headers $headers
         $response | Out-File -FilePath $OutFile -Encoding utf8
+
         Write-Output "Data export successful. File saved as $OutFile."
     } catch {
+        Write-Error "Error message: $($_.Exception.Message)"
+        Write-Output ""
         Write-Error "Failed to export data: $_"
+        Write-Output ""
+        Write-Error "Stack trace: $($_.Exception.StackTrace)"
     }
 }
