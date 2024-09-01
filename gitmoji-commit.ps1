6a05a3a0bf359ad6e1585c7fe510c3ec73991786
Colin Williams <colin.williams.dev@gmail.com>
2024-09-01 01:07:10 -0500

    :sparkles:: gitmoji-commit script that parses json for emoji shortcode by arg[0] and runs git commit m with arg[1]

--- gitmoji-commit.ps1
+++ gitmoji-commit.ps1
@@ -1,16 +1,28 @@
-# Load the JSON file
+# Script takes 2 arguments (args[0] defaults to ":computer::") for gitmoji commit standard and makes git commit -m ":computer:: your message..."
+
+if (-not $args[0]) {
+    $commitType = "chore"
+} else {
+    $commitType = $args[0]
+}
+
+if (-not $args[1]) {
+    Write-Host "Please provide a commit message after your commit type argument..."
+    exit 1
+} else {
+    $commitMessage  = $args[1]
+}
+
 $emojiData = Get-Content -Raw -Path "E:\Dev\profiles\gitmoji.json" | ConvertFrom-Json
 
-# Get the commit type/intention from the command line argument
-$commitType = $args[0]
-
-# Get the corresponding emoji short-code
 $emoji = $emojiData.$commitType
 
-# If emoji exists, copy to clipboard
+$output = ""
+
 if ($emoji) {
-    $emoji | Set-Clipboard
-    Write-Host "$emoji copied to clipboard!"
+    $output = "$emoji $commitMessage"
+    git commit -m $output
 } else {
     Write-Host "No emoji found matching commit type: $commitType"
+    exit 1
 }
